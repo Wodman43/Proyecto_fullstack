e8e1ffd3b90a2e239c12ab7501f062fd27177755
wodman43 <lopezquira10gmail.com>
2023-09-06 23:08:52 -0500

    no quiere funcionar en mi pc

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-tasks'
+const URI = 'mongodb://127.0.0.1/mern-tasks';
+// const URI = 'mongodb://localhost/mern-tasks'
 mongoose.connect(URI)
     .then(db=> console.log('DB si conecta'))
     .catch(err => console.error(err));
